ca5cfd4293d51537177505f8dad51935f002867f
dophuochung2428 <dophuochung2428@gmail.com>
2025-11-24 17:14:51 +0700

    feat(features): refactoring server queries

--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -1,23 +1,13 @@
-import { cookies } from "next/headers";
-import { Databases, Client, Query, Account } from "node-appwrite";
-import { AUTH_COOKIE } from "@/features/auth/constants";
+import { Query } from "node-appwrite";
 import { DATABASE_ID, MEMBER_ID, WORKSPACES_ID } from "@/config";
 import { getMember } from "@/features/members/utils";
 import { Workspace } from "./types";
+import { createSessionClient } from "@/lib/appwrite";
 
 export const getWorkspaces = async () => {
     try {
-        const client = new Client()
-            .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-            .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+        const { databases, account } = await createSessionClient();
 
-        const session = await (await cookies()).get(AUTH_COOKIE);
-
-        if (!session) return { documents: [], total: 0 };
-
-        client.setSession(session.value);
-        const databases = new Databases(client);
-        const account = new Account(client);
         const user = await account.get();
 
         const members = await databases.listDocuments(
@@ -53,17 +43,8 @@ interface GetWorkspaceProps {
 
 export const getWorkspace = async ({ workspaceId }: GetWorkspaceProps) => {
     try {
-        const client = new Client()
-            .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-            .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+        const { databases, account } = await createSessionClient();
 
-        const session = await (await cookies()).get(AUTH_COOKIE);
-
-        if (!session) return null;
-
-        client.setSession(session.value);
-        const databases = new Databases(client);
-        const account = new Account(client);
         const user = await account.get();
 
         const member = await getMember({
