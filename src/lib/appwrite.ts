b56cea95dde8cd390c4a0ae3a450f95c16363957
dophuochung2428 <dophuochung2428@gmail.com>
2025-11-29 01:23:02 +0700

    feat(members): add update and delete function

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -42,5 +42,8 @@ export async function createAdminClient() {
         get account() {
             return new Account(client);
         },
+        get users(){
+            return new Users(client)
+        }
     };
 };
