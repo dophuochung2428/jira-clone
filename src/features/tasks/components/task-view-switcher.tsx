5b8b21ce08fb058ce3308cd1fed22b406c4bd039
dophuochung2428 <dophuochung2428@gmail.com>
2025-12-08 16:33:38 +0700

    feat(tasks): building data table

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -15,6 +15,8 @@ import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import { useQueryState } from "nuqs";
 import { DataFilters } from "./data-filters";
 import { useTaskFilters } from "../hooks/use-task-filters";
+import { DataTable } from "./data-table";
+import { columns } from "./columns";
 
 export const TaskViewSwitcher = () => {
     const [{
@@ -88,7 +90,7 @@ export const TaskViewSwitcher = () => {
                 ) : (
                     <>
                         <TabsContent value="table" className="mt-0">
-                            {JSON.stringify(tasks)}
+                            <DataTable columns={columns} data={tasks?.documents ?? []} />
                         </TabsContent>
                         <TabsContent value="kanban" className="mt-0">
                             {JSON.stringify(tasks)}
